cc8b7b3e0463541c43f1537486d5ec9f492e3bd7
upFPS <73418250+Evgencheg@users.noreply.github.com>
2026-01-25 21:28:08 +0300

    фикс

--- Content.Server/_Wega/Mining/MiningConsoleSystem.cs
+++ Content.Server/_Wega/Mining/MiningConsoleSystem.cs
@@ -107,7 +107,7 @@ public sealed class MiningConsoleSystem : EntitySystem
 
         if (account.Credits >= 1)
         {
-            _stack.Spawn((int)account.Credits, Credit, Transform(entity).Coordinates);
+            _stack.SpawnAtPosition((int)account.Credits, Credit, Transform(entity).Coordinates);
             account.Credits = 0;
         }
 
