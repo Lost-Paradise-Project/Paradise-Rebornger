95b2cc26e7f93a79aa280599cc5dea4921cfb7e9
upFPS <73418250+Evgencheg@users.noreply.github.com>
2026-01-28 00:17:04 +0300

    GPS

---
+++ Content.Shared/_GoobStation/CCVars/CCVars.Goob.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.Configuration;
+
+namespace Content.Shared._GoobStation.CCVar;
+
+[CVarDefs]
+public sealed partial class GoobCVars
+{
+
+    /// <summary>
+    /// Controls how often GPS updates.
+    /// </summary>
+    public static readonly CVarDef<float> GpsUpdateRate =
+        CVarDef.Create("gps.update_rate", 1f, CVar.SERVER | CVar.REPLICATED);
+}
