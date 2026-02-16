ff37ab41dcbba1b2e114f0d78eb486bf3943c5c2
SpicyDarkFox <foxyflutter6@gmail.com>
2026-02-16 06:16:24 +0300

    небольшой фикс очереди

--- Content.Server/Connection/ConnectionManager.cs
+++ Content.Server/Connection/ConnectionManager.cs
@@ -68,7 +68,6 @@ namespace Content.Server.Connection
         [Dependency] private readonly IHttpClientHolder _http = default!;
         [Dependency] private readonly IAdminManager _adminManager = default!;
         [Dependency] private readonly IEntityManager _entityManager = default!;
-        private ISharedSponsorsManager? _sponsorsMgr; // Corvax-Sponsors
 
         private GameTicker? _ticker;
 
@@ -313,8 +312,11 @@ namespace Content.Server.Connection
             }
 
             // Corvax-Queue-Start
-            var isQueueEnabled = IoCManager.Instance!.TryResolveType<IServerJoinQueueManager>(out var mgr) && mgr.IsEnabled;
-            if ((softPlayerCount >= _cfg.GetCVar(CCVars.SoftMaxPlayers) && !adminBypass) && !wasInGame && !isQueueEnabled)
+            var isQueueEnabled = _cfg.GetCVar(Content.Server._NC.CCCvars.CCCVars.QueueEnabled); //LP edit start
+            var isprivileger = await HavePrivilegedJoin(userId);
+            if (isQueueEnabled || isprivileger)
+                return null;    //LP edit end
+            else if (softPlayerCount >= _cfg.GetCVar(CCVars.SoftMaxPlayers) && !adminBypass && !wasInGame)
             // Corvax-Queue-End
             {
                 return (ConnectionDenyReason.Full, Loc.GetString("soft-player-cap-full"), null);
