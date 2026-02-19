fb6f1d93ea98fee3071063c1a3e3d4fdd43ea3ce
SanitarDurkovic <sanitardurkovic8@gmail.com>
2026-02-20 00:01:26 +0300

    Apply suggestions from code review
    Co-authored-by: Evgencheg <73418250+Evgencheg@users.noreply.github.com>

--- Content.Client/Silicons/Borgs/BorgSwitchableTypeSystem.cs
+++ Content.Client/Silicons/Borgs/BorgSwitchableTypeSystem.cs
@@ -17,7 +17,7 @@ public sealed class BorgSwitchableTypeSystem : SharedBorgSwitchableTypeSystem
     [Dependency] private readonly BorgSystem _borgSystem = default!;
     [Dependency] private readonly AppearanceSystem _appearance = default!;
     [Dependency] private readonly SpriteSystem _sprite = default!;
-    [Dependency] private readonly IResourceCache _resourceCache = default!; //LP edit
+    [Dependency] private readonly IResourceCache _resourceCache = default!; // LP edit
 
     public override void Initialize()
     {
