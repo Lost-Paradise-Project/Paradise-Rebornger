cbcd74432c38cbc8ddc38722bd0508da31c20e4d
SpicyDarkFox <foxyflutter6@gmail.com>
2026-03-06 11:04:38 +0300

    Добавление настроек для спонсорки

--- Content.Shared/_LP/LPCvars.cs
+++ Content.Shared/_LP/LPCvars.cs
@@ -7,4 +7,20 @@ public sealed class LPCvars
 {
     public static readonly CVarDef<string> DiscordBanWebhook =
         CVarDef.Create("discord.ban_webhook", "", CVar.SERVERONLY);
+
+
+    #region Cheats
+    /// <summary>
+    /// Переключатель читов.
+    /// </summary>
+    // public static readonly CVarDef<bool> EnableCheats =
+    //     CVarDef.Create("cheats.enabled", false, CVar.SERVER | CVar.CHEAT);
+
+    /// <summary>
+    /// Позволяет указать автоматически выдаваемый ВСЕМ игрокам уровень спонсорки.
+    /// Использовать только для тестирования.
+    /// </summary>
+    public static readonly CVarDef<int> SponsorLevelHack =
+        CVarDef.Create("cheats.sponsorlevel_hack", 0, CVar.REPLICATED | CVar.CHEAT | CVar.NOTIFY);
+    #endregion
 }
