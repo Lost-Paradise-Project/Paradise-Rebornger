e19db67800aa53a4f86ded2401447f77cc20c428
upFPS <73418250+Evgencheg@users.noreply.github.com>
2026-03-22 22:47:48 +0300

    имбуля

--- Content.Server/Light/EntitySystems/EmergencyLightSystem.cs
+++ Content.Server/Light/EntitySystems/EmergencyLightSystem.cs
@@ -5,6 +5,9 @@ using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
 using Content.Server.Station.Systems;
 using Content.Shared.Examine;
+using Robust.Shared.Player;
+using Robust.Shared.Timing;
+using Robust.Server.Audio;
 using Content.Shared.Light;
 using Content.Shared.Light.Components;
 using Content.Shared.Power;
@@ -22,6 +25,8 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
     [Dependency] private readonly PointLightSystem _pointLight = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
     [Dependency] private readonly StationSystem _station = default!;
+    [Dependency] private readonly AudioSystem _audioSystem = default!; // Orion
+    [Dependency] private readonly IGameTiming _timing = default!; // Orion
 
     public override void Initialize()
     {
@@ -31,7 +36,10 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
         SubscribeLocalEvent<AlertLevelChangedEvent>(OnAlertLevelChanged);
         SubscribeLocalEvent<EmergencyLightComponent, ExaminedEvent>(OnEmergencyExamine);
         SubscribeLocalEvent<EmergencyLightComponent, PowerChangedEvent>(OnEmergencyPower);
+        SubscribeLocalEvent<EmergencyLightComponent, MapInitEvent>(OnMapInit); // Orion
     }
+
+    private void OnMapInit(Entity<EmergencyLightComponent> entity, ref MapInitEvent args) => UpdateState(entity); // Orion
 
     private void OnEmergencyPower(Entity<EmergencyLightComponent> entity, ref PowerChangedEvent args)
     {
@@ -82,9 +90,9 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
         {
             case EmergencyLightState.On:
             case EmergencyLightState.Charging:
+            case EmergencyLightState.Full:
                 EnsureComp<ActiveEmergencyLightComponent>(uid);
                 break;
-            case EmergencyLightState.Full:
             case EmergencyLightState.Empty:
                 RemComp<ActiveEmergencyLightComponent>(uid);
                 break;
@@ -109,6 +117,7 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
 
             _pointLight.SetColor(uid, details.EmergencyLightColor, pointLight);
             _appearance.SetData(uid, EmergencyLightVisuals.Color, details.EmergencyLightColor, appearance);
+            UpdateAlarmSound((uid, light), details);
 
             if (details.ForceEnableEmergencyLights && !light.ForciblyEnabled)
             {
@@ -132,39 +141,44 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
         RaiseLocalEvent(uid, new EmergencyLightEvent(state));
     }
 
+    // Orion-Edit-Start
     public override void Update(float frameTime)
     {
         var query = EntityQueryEnumerator<ActiveEmergencyLightComponent, EmergencyLightComponent, BatteryComponent>();
         while (query.MoveNext(out var uid, out _, out var emergencyLight, out var battery))
         {
-            Update((uid, emergencyLight), battery, frameTime);
-        }
-    }
-
-    private void Update(Entity<EmergencyLightComponent> entity, BatteryComponent battery, float frameTime)
-    {
-        if (entity.Comp.State == EmergencyLightState.On)
-        {
-            if (!_battery.TryUseCharge((entity.Owner, battery), entity.Comp.Wattage * frameTime))
+            if (emergencyLight.State == EmergencyLightState.On)
             {
-                SetState(entity.Owner, entity.Comp, EmergencyLightState.Empty);
-                TurnOff(entity);
-            }
-        }
-        else
-        {
-            _battery.ChangeCharge((entity.Owner, battery), entity.Comp.ChargingWattage * frameTime * entity.Comp.ChargingEfficiency);
-            if (_battery.IsFull((entity.Owner, battery)))
-            {
-                if (TryComp<ApcPowerReceiverComponent>(entity.Owner, out var receiver))
+                if (!_battery.TryUseCharge((uid, battery), emergencyLight.Wattage * frameTime))
                 {
-                    receiver.Load = 1;
+                    SetState(uid, emergencyLight, EmergencyLightState.Empty);
+                    TurnOff((uid, emergencyLight));
                 }
-
-                SetState(entity.Owner, entity.Comp, EmergencyLightState.Full);
             }
+            else
+            {
+                _battery.ChangeCharge((uid, battery), emergencyLight.ChargingWattage * frameTime * emergencyLight.ChargingEfficiency);
+
+                if (_battery.IsFull((uid, battery)))
+                {
+                    if (TryComp<ApcPowerReceiverComponent>(uid, out var receiver))
+                    {
+                        receiver.Load = 1;
+                    }
+
+                    SetState(uid, emergencyLight, EmergencyLightState.Full);
+                }
+            }
+
+            if (emergencyLight.AlarmSound == null || _timing.CurTime < emergencyLight.AlarmNextSound)
+                continue;
+
+            _audioSystem.PlayEntity(emergencyLight.AlarmSound, Filter.Pvs(uid, 0.5f), uid, true);
+
+            emergencyLight.AlarmNextSound = _timing.CurTime.Add(emergencyLight.AlarmInterval);
         }
     }
+    // Orion-Edit-End
 
     /// <summary>
     ///     Updates the light's power drain, battery drain, sprite and actual light state.
@@ -189,15 +203,19 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
             TurnOff(entity, details.Color);
             SetState(entity.Owner, entity.Comp, EmergencyLightState.Charging);
         }
-        else if (!receiver.Powered) // If internal battery runs out it will end in off red state
+        else if (!receiver.Powered) // If internal battery runs out
         {
-            TurnOn(entity, Color.Red);
+            // Orion-Edit-Start
+            if (!entity.Comp.ForciblyEnabled)
+                TurnOn(entity, Color.Red);
+            // Orion-Edit-End
             SetState(entity.Owner, entity.Comp, EmergencyLightState.On);
         }
         else // Powered and enabled
         {
             TurnOn(entity, details.Color);
             SetState(entity.Owner, entity.Comp, EmergencyLightState.On);
+            UpdateAlarmSound(entity, details); // Orion
         }
     }
 
@@ -238,4 +256,23 @@ public sealed class EmergencyLightSystem : SharedEmergencyLightSystem
         _appearance.SetData(entity.Owner, EmergencyLightVisuals.On, true);
         _ambient.SetAmbience(entity.Owner, true);
     }
+
+    // Orion-Start
+    private void UpdateAlarmSound(Entity<EmergencyLightComponent> entity, AlertLevelDetail alertLevel)
+    {
+        if (alertLevel.AlarmSound == null)
+        {
+            entity.Comp.AlarmSound = null;
+            return;
+        }
+
+        entity.Comp.AlarmSound = alertLevel.AlarmSound;
+        entity.Comp.AlarmInterval = alertLevel.AlarmInterval;
+
+        if (entity.Comp.AlarmInterval < TimeSpan.FromSeconds(1f)) // Safeguard against spam and client crash
+            entity.Comp.AlarmInterval = TimeSpan.FromSeconds(1f);
+
+        entity.Comp.AlarmNextSound = _timing.CurTime.Add(entity.Comp.AlarmInterval);
+    }
+    // Orion-End
 }
