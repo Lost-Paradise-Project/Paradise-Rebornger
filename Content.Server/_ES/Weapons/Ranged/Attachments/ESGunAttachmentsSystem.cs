7fee1fec3ce01c51bc36cc2ff452b53e06ff4a87
upFPS <73418250+Evgencheg@users.noreply.github.com>
2026-03-25 19:39:58 +0300

    Revert "Тюнинг пушек"
    This reverts commit 8a99eea5e1eee52a21b21a12c1e25e414a975047.

--- Content.Server/_ES/Weapons/Ranged/Attachments/ESGunAttachmentsSystem.cs
+++
@@ -1,5 +0,0 @@
-﻿using Content.Shared._ES.Weapons.Ranged.Attachments;
-
-namespace Content.Server._ES.Weapons.Ranged.Attachments;
-
-public sealed class ESGunAttachmentsSystem : ESSharedGunAttachmentsSystem;
--- Content.Shared/_ES/Weapons/Ranged/Attachments/Components/ESGunAttachmentComponent.cs
+++
@@ -1,7 +0,0 @@
-﻿using Robust.Shared.GameStates;
-
-namespace Content.Shared._ES.Weapons.Ranged.Attachments.Components;
-
-[RegisterComponent, NetworkedComponent]
-[Access(typeof(ESSharedGunAttachmentsSystem))]
-public sealed partial class ESGunAttachmentComponent : Component;
