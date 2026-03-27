fd4dcbeaf9047dfdd81a721841830414911570e9
upFPS <73418250+Evgencheg@users.noreply.github.com>
2026-03-27 14:24:58 +0300

    фиксы

--- Content.Shared/Localizations/ContentLocalizationManager.cs
+++ Content.Shared/Localizations/ContentLocalizationManager.cs
@@ -122,8 +122,8 @@ namespace Content.Shared.Localizations
             {
                 <= 0 => string.Empty,
                 1 => list[0],
-                2 => $"{list[0]} and {list[1]}",
-                _ => $"{string.Join(", ", list.GetRange(0, list.Count - 1))}, and {list[^1]}"
+                2 => $"{list[0]} и {list[1]}", // Corvax-Localization
+                _ => $"{string.Join(", ", list.GetRange(0, list.Count - 1))}, и {list[^1]}" // Corvax-Localization
             };
         }
 
@@ -136,8 +136,8 @@ namespace Content.Shared.Localizations
             {
                 <= 0 => string.Empty,
                 1 => list[0],
-                2 => $"{list[0]} or {list[1]}",
-                _ => $"{string.Join(", ", list.GetRange(0, list.Count - 1))}, or {list[^1]}"
+                2 => $"{list[0]} или {list[1]}", // Corvax-Localization
+                _ => $"{string.Join(", ", list.GetRange(0, list.Count - 1))}, или {list[^1]}" // Corvax-Localization
             };
         }
 
