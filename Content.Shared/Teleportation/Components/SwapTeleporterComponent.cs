3d967d85aa1f0c966000eb2ec169870e7ac88e57
BL02DL <ivanov.j970@gmail.com>
2026-04-19 13:07:56 +0700

    Fix

--- Content.Shared/Teleportation/Components/SwapTeleporterComponent.cs
+++ Content.Shared/Teleportation/Components/SwapTeleporterComponent.cs
@@ -44,7 +44,7 @@ public sealed partial class SwapTeleporterComponent : Component
     /// A minimum waiting period inbetween teleports.
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
-    public TimeSpan Cooldown = TimeSpan.FromMinutes(1); // LP Edit 5 -> 1
+    public TimeSpan Cooldown = TimeSpan.FromMinutes(5);
 
     /// <summary>
     /// Sound played when teleportation begins
