644bc412ee644a6d77171e5ff32915e7d11a3b9e
SanitarDurkovic <sanitardurkovic8@gmail.com>
2026-08-19 20:04:18 +0300

    1

--- Content.Server/_Orion/Ghost/GhostReturnToRoundSystem.cs
+++ Content.Server/_Orion/Ghost/GhostReturnToRoundSystem.cs
@@ -10,6 +10,7 @@ using Content.Shared.Ghost;
 using Robust.Server.Player;
 using Robust.Shared.Console;
 using Robust.Shared.Player;
+using Robust.Shared.Timing; /// LP edit
 
 namespace Content.Server._Orion.Ghost;
 
@@ -21,14 +22,17 @@ public sealed class GhostReturnToRoundSystem : SharedGhostReturnToRoundSystem
     [Dependency] private readonly IConsoleHost _console = default!;
     [Dependency] private readonly IAdminLogManager _adminLogger = default!;
     [Dependency] private readonly SharedGhostSystem _ghostSystem = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!; /// LP edit
 
     private int _ghostRespawnMaxPlayers;
+    private readonly Dictionary<EntityUid, TimeSpan> _returnToRoundTimes = []; /// LP edit
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeNetworkEvent<GhostReturnToRoundRequest>(OnGhostReturnToRoundRequest);
+        SubscribeLocalEvent<GhostReturnToRoundTimerStartEvent>(ReturnToRoundTimer); /// LP edit
 
         Cfg.OnValueChanged(OCCVars.GhostRespawnMaxPlayers,
             ghostRespawnMaxPlayers =>
@@ -39,6 +43,29 @@ public sealed class GhostReturnToRoundSystem : SharedGhostReturnToRoundSystem
 
         _console.RegisterCommand("returntoround", ReturnToRoundCommand, ReturnToRoundCompletion);
     }
+
+    /// LP edit start
+    /// <summary>
+    /// The timer for the player to return to the lobby.
+    /// Таймер возврата игрока в лобби.
+    /// </summary>
+    private void ReturnToRoundTimer(GhostReturnToRoundTimerStartEvent ev)
+    {
+        var timeFromStartRound = _gameTiming.CurTime.Subtract(_gameTicker.RoundStartTimeSpan);
+        // A little bit of offset won’t hurt, since that is what the founding fathers intended. Немного смещения не помешает, ведь именно так и задумывали отцы-основатели).
+        var timeOffset = TimeSpan.Zero;
+        if (timeFromStartRound > TimeSpan.FromHours(1)) // The time offset begins with the first hour of the shift. Смещение времени начинается с одного часа смены.
+            timeOffset = TimeSpan.FromHours(Math.Min((timeFromStartRound.TotalHours - 1) / 3 * 10, 10));
+
+        var returnTime = GameTiming.CurTime + GhostRespawnTime + timeOffset; // We calculate the time when the player can respawn. Вычисляем время, когда игрок может зареспавниться.
+        _returnToRoundTimes[ev.Ghost] = returnTime; // Save the time on specific ghost. Сохраняем время на конкретного призрака.
+
+        if (!_playerManager.TryGetSessionByEntity(ev.Ghost, out var session)) // We are looking for the player who owns the ghost. Шукаемо игрока, которому принадлежит призрак.
+            return;
+
+        RaiseNetworkEvent(new GhostReturnToRoundSendTimerEvent(returnTime), session.Channel); // Found it, sending the time to the player. Нашли, отправляем время игроку.
+    }
+    /// LP edit end
 
     private void TryGhostReturnToRound(EntityUid uid, Entity<GhostComponent> ent)
     {
@@ -56,28 +83,38 @@ public sealed class GhostReturnToRoundSystem : SharedGhostReturnToRoundSystem
             return;
         }
 
-        var now = GameTiming.CurTime;
-        var timeOffset = now - ent.Comp.TimeOfDeath;
+        /// LP edit start
+        // var now = GameTiming.CurTime;
+        // var timeOffset = now - ent.Comp.TimeOfDeath;
 
-        if (timeOffset < TimeSpan.Zero)
-        {
-            Entity<GhostComponent?> ghostEnt = (ent.Owner, ent.Comp);
-            _ghostSystem.SetTimeOfDeath(ghostEnt, now);
-            timeOffset = TimeSpan.Zero;
-        }
+        // if (timeOffset < TimeSpan.Zero)
+        // {
+        //     Entity<GhostComponent?> ghostEnt = (ent.Owner, ent.Comp);
+        //     _ghostSystem.SetTimeOfDeath(ghostEnt, now);
+        //     timeOffset = TimeSpan.Zero;
+        // }
 
-        if (timeOffset < GhostRespawnTime)
+        // if (timeOffset < GhostRespawnTime)
+
+        if (!_returnToRoundTimes.TryGetValue(uid, out var returnTime))
+            return;
+
+        var timeLeft = returnTime - GameTiming.CurTime;
+
+        if (timeLeft > TimeSpan.Zero)
+        /// LP edit end
         {
             SendChatMsg(session,
-                Loc.GetString("ghost-respawn-time-left", ("time", (GhostRespawnTime - timeOffset).ToString()))
+            Loc.GetString("ghost-respawn-time-left", ("time", timeLeft.ToString())) /// LP edit (GhostRespawnTime - timeOffset) > timeLeft
             );
             return;
         }
 
+        _returnToRoundTimes.Remove(uid); /// LP edit
         _gameTicker.Respawn(session);
         _adminLogger.Add(LogType.Mind, LogImpact.Medium, $"{Loc.GetString("ghost-respawn-log-return-to-lobby", ("userName", session.Name))}");
 
-        var message= Loc.GetString("ghost-respawn-window-rules-footer");
+        var message = Loc.GetString("ghost-respawn-window-rules-footer"); /// LP edit
         SendChatMsg(session, message);
     }
 
--- Content.Shared/_Orion/Ghost/SharedGhostReturnToRoundSystem.cs
+++ Content.Shared/_Orion/Ghost/SharedGhostReturnToRoundSystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared._Orion.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Timing;
+using Robust.Shared.Serialization; /// LP edit
 
 namespace Content.Shared._Orion.Ghost;
 
@@ -23,3 +24,24 @@ public abstract class SharedGhostReturnToRoundSystem : EntitySystem
 
     protected TimeSpan GhostRespawnTime = new(0, 5, 0);
 }
+
+/// LP edit start
+/// <summary>
+/// It is responsible for transmitting to the player the time from the server when they can return to the round.
+/// Отвечает за передачу игроку времени с сервера, когда тому можно вернуться в раунд.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class GhostReturnToRoundSendTimerEvent(TimeSpan returnTime) : EntityEventArgs
+{
+    public TimeSpan ReturnTime { get; } = returnTime;
+}
+
+/// <summary>
+/// The server notifies that the return to round timer needs to be started for a specific player.
+/// Уведомляет сервер о том, что надо запустить таймер возврата в раунд для конкретного игрока.
+/// </summary>
+public sealed class GhostReturnToRoundTimerStartEvent(EntityUid ghost) : EntityEventArgs
+{
+    public EntityUid Ghost { get; } = ghost;
+}
+/// LP edit end
