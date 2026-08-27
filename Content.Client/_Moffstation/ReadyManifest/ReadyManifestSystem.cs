32996e1d160967561c826b7f74f59087508bfbd7
SanitarDurkovic <sanitardurkovic8@gmail.com>
2026-08-27 16:53:55 +0300

    3

--- Content.Client/_Moffstation/ReadyManifest/ReadyManifestSystem.cs
+++ Content.Client/_Moffstation/ReadyManifest/ReadyManifestSystem.cs
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
-//
-// SPDX-License-Identifier: MIT
-
 using Content.Shared._Moffstation.ReadyManifest;
 
 namespace Content.Client._Moffstation.ReadyManifest;
